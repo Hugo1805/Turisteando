a82b2e205869bd1e1a0552a3077afd7e0ee3a710
Soul <51276346+Hugo1805@users.noreply.github.com>
2019-10-19 22:32:05 -0500

    se agrego la clase de usuario prueba, el metodo get para gia y el metodo post (Falta acomodar pq inserta en null), tambien cuenta con la Conexion a BD

--- src/pe/com/turisteando/rest/dto/usuarioDTO.java
+++ src/pe/com/turisteando/rest/dto/usuarioDTO.java
@@ -1,5 +1,34 @@
 package pe.com.turisteando.rest.dto;
 
-public class usuarioDTO {
+import java.io.Serializable;
 
+public class usuarioDTO implements Serializable {
+	
+	private String usuario;
+	private String clave ;
+
+	public usuarioDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public usuarioDTO(String usuario, String clave) {
+		super();
+		this.usuario = usuario;
+		this.clave = clave;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	
+	
 }
